e9706d9da83545d65daf1febfd86d3feeee0ee9a
PrajwalTarawade <prajwal.tarawade@capgemini.com>
2021-05-24 16:43:32 +0530

    OFDA front-end

--- Online-Food-Delivery-Application/src/main/java/com/cg/fds/controller/BillController.java
+++ Online-Food-Delivery-Application/src/main/java/com/cg/fds/controller/BillController.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +14,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.fds.entities.Bill;
